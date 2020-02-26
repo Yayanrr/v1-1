3f33a87b151f005e3f98890ad52000d251755f33
msrizal <60194034+msrizal@users.noreply.github.com>
2020-02-26 08:38:22 +0700

    Update cing.py

--- cing.py
+++ cing.py
@@ -96,8 +96,9 @@ def on_message(ws, message):
         if evn == 'live_shadowjoin':
             if status == 'bangun':
                 ws.send(lsjoin)
-        if evn == 'live_likee' and status == 'bangun':
-            ws.send(likee)
+        if evn == 'live_join'
+            If status == 'likee':
+                 ws.send(likee)
         if evn == 'live_message' and psn == '!off' and status == 'bangun':
             status = 'tidur'
             ws.send(tidur)
@@ -172,8 +173,8 @@ def on_message(ws, message):
         if evn == 'live_message' and psn == '!on' and status == 'tidur':
             status = 'bangun'
             ws.send(bangun)
-        if evn == 'live_message' and pan == '!like‏‏‎' and status == 'tidur':
-            status == 'bangun'
+        if evn == 'live_message' and psn == '!like‏‏‎' and status == 'tidur':
+            status == 'likes'
             ws.send(likes)
         if evn == 'live_message' and psn[:1].lower() == 'w' and pan[-1:] == 'k':
             ws.send(ping)
